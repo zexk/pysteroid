cd1ea2570eec34ecc491cc45e4ae5e56d0ca41bc
Zexk <bouraoui@outlook.it>
2025-09-26 15:48:38 +0200

    add player movement

--- main.py
+++ main.py
@@ -9,17 +9,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+                
         screen.fill((0,0,0))
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
         dt = (clock.tick(60)) / 1000
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -17,4 +17,22 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-       
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
