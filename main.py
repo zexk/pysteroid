56fdb1fd8f2940599c3261db5e4a9e8cb37f2fc8
Zexk <bouraoui@outlook.it>
2025-09-26 13:47:04 +0200

    implement game loop

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
+from constants import * 
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
+
 
 if __name__ == "__main__":
     main()
