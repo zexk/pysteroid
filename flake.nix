2b9c9addf4bcfdaca37534a361f82f970e60d4f9
Zexk <bouraoui@outlook.it>
2025-09-26 15:33:51 +0200

    new devshell

--- flake.nix
+++ flake.nix
@@ -1,76 +1,15 @@
 {
-  description = "uv2nix devshell";
+  description = "pysteroid dev flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-
-    pyproject-nix = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    inputs@{
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        workspaceRoot = ./.;
-        venvName = "venv";
-        python = pkgs.python313;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        workspace = inputs.uv2nix.lib.workspace.loadWorkspace { workspaceRoot = workspaceRoot; };
-        overlay = workspace.mkPyprojectOverlay {
-          sourcePreference = "wheel";
-        };
-        baseSet = pkgs.callPackage inputs.pyproject-nix.build.packages {
-          inherit python;
-        };
-        pythonSet = baseSet.overrideScope (
-          pkgs.lib.composeManyExtensions [
-            inputs.pyproject-build-systems.overlays.default
-            overlay
-          ]
-        );
-        venv = pythonSet.mkVirtualEnv "${venvName}" workspace.deps.default;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            python
-            pkgs.uv
-            venv
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      packages = with pkgs; [ uv (python313.withPackages (ps: [ ps.pygame ])) ];
+    };
+  };
 }
